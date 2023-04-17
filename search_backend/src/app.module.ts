c5cf01110814f8c19e4da0692575d39d3f64fe7c
Felipe Mori <felipe.mori@onfriday.com.br>
2023-04-17 00:06:18 -0300

    feat(API): Mongoose implementation

--- search_backend/src/app.module.ts
+++ search_backend/src/app.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { ProductsController } from './products/products.controller';
 import { PuppeteerModule } from 'nest-puppeteer';
 import { ScrapingService } from './scraping/scraping.service';
+import { SearchService } from './search/search.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SearchSchema } from './search/search.schema';
 
 @Module({
-  imports: [PuppeteerModule.forRoot()],
+  imports: [
+    PuppeteerModule.forRoot(),
+    MongooseModule.forRoot(
+      'mongodb+srv://fmatheusog:Pjd5anuEf49X2KLZ@db-product-search.k4xbzil.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    MongooseModule.forFeature([
+      {
+        name: 'Search',
+        schema: SearchSchema,
+      },
+    ]),
+  ],
   controllers: [ProductsController],
-  providers: [ScrapingService],
+  providers: [ScrapingService, SearchService],
 })
 export class AppModule {}
