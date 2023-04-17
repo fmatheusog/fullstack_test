bfae345c039fa7f40f101ffc08aace7482d244cc
Felipe Mori <felipe.mori@onfriday.com.br>
2023-04-16 22:16:26 -0300

    chore(web): add image domains to next config

--- search_frontend/next.config.js
+++ search_frontend/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'http2.mlstatic.com',
+      'i.zst.com.br',
+    ]
+  }
 }
 
 module.exports = nextConfig
